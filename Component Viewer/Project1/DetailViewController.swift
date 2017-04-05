7c60433832f08a550076817e33178a9b0bd70e7b
Corey *Brice Wyatt <bricewyatt9@gmail.com>
2017-04-05 07:28:59 -0500

    Add files via upload

---
+++ Component Viewer/Project1/DetailViewController.swift
@@ -0,0 +1,72 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by mad mac on 3/24/17.
+//  Copyright (c) 2017 Treya. All rights reserved.
+//
+
+import Social
+import UIKit
+
+class DetailViewController: UIViewController {
+
+   
+    @IBOutlet weak var detailImageView: UIImageView!
+
+
+    var detailItem: String? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail: AnyObject = self.detailItem {
+            if let imageView = self.detailImageView {
+                imageView.image = UIImage(named: detail as! String)
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Action, target: self, action: "shareTapped")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    func shareTapped() {
+        let vc = UIActivityViewController(activityItems: [detailImageView.image!], applicationActivities: [])
+        presentViewController(vc, animated: true, completion: nil)
+    }
+    
+    
+//        let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+//        vc.setInitialText("Look at this great picture!")
+//        vc.addImage(detailImageView.image!)
+//        vc.addURL(NSURL(string: "http://www.photolib.noaa.gov/nssl"))
+//        presentViewController(vc, animated: true, completion: nil)
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Component Viewer/Project1/MasterViewController.swift
@@ -0,0 +1,83 @@
+//
+//  MasterViewController.swift
+//  Project1
+//
+//  Created by mad mac on 3/24/17.
+//  Copyright (c) 2017 Treya. All rights reserved.
+//
+
+import UIKit
+
+class MasterViewController: UITableViewController {
+
+    var objects = [AnyObject]()
+
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let fm = NSFileManager.defaultManager()
+        let path = NSBundle.mainBundle().resourcePath!
+        let items = fm.contentsOfDirectoryAtPath(path, error: nil)
+        
+        for item in items as! [String] {
+            if item.hasPrefix("nssl") {
+                objects.append(item)
+            }
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func insertNewObject(sender: AnyObject) {
+        objects.insert(NSDate(), atIndex: 0)
+        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+    }
+
+    // MARK: - Segues
+
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "showDetail" {
+            if let indexPath = self.tableView.indexPathForSelectedRow() {
+                let detailViewController = segue.destinationViewController as! DetailViewController
+                detailViewController.detailItem = (objects[indexPath.row] as! String)
+            }
+        }
+    }
+
+    // MARK: - Table View
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return objects.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! UITableViewCell
+
+        let object: (AnyObject) = objects[indexPath.row]
+        cell.textLabel!.text = (object as! String)
+        return cell
+    }
+
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+
+
+
+
+}
+
